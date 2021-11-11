4588e29d7db74d01653678730f7f50b77b183157
ScoffW <stivie333@mail.ru>
2021-11-11 23:02:17 +0300

    Initial commit

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int price = 3250;
-        int bon = 20;
-        int bonus = price / bon;
-        System.out.println("Итоговый бонус: " + bonus);
-    }
-}
